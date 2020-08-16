00bfed1d319007451f6a896e6207f681d60eeebb
kelaban <kelaban17@gmail.com>
2020-08-16 17:05:09 +0000

    Deploying to gh-pages from  @ 543b3e46627b806d73cffad91b17a247d770b17d 🚀

---
+++ precache-manifest.b2b5d20a22a74c5262e410327e01c55f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08697ccb121f51a41ade3d5e867e321",
+    "url": "/xwordmaker/index.html"
+  },
+  {
+    "revision": "656a957a25193e4829ae",
+    "url": "/xwordmaker/static/css/main.85897755.chunk.css"
+  },
+  {
+    "revision": "4662e3ad0f57fbd2190d",
+    "url": "/xwordmaker/static/js/2.e55a3d32.chunk.js"
+  },
+  {
+    "revision": "656a957a25193e4829ae",
+    "url": "/xwordmaker/static/js/main.96c19d5c.chunk.js"
+  },
+  {
+    "revision": "1dc39b8bc1599b98680f",
+    "url": "/xwordmaker/static/js/runtime-main.e4fdeecd.js"
+  }
+]);
